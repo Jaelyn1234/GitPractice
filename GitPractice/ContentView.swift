4de538b42c582dc980ab16865816217e522b7d7d
Jaelyn <jendiep@yahoo.com>
2025-08-06 11:23:19 -0400

    Changed text to "Cookies!" and uploaded image of cookies.

--- GitPractice/ContentView.swift
+++ GitPractice/ContentView.swift
@@ -10,9 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("Change 1!")
+            Text("Cookies!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color(hue: 0.102, saturation: 0.86, brightness: 0.724))
+            Image("cookie")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .padding(.all, 20.0)
         }
-        .padding()
     }
 }
 
